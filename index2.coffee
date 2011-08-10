ae7dbae7b9a6ec1a96789b0fc1b07061b1033eae
drewlesueur <drewalex@gmail.com>
2011-08-10 09:19:38 +0000

    no polymorphism test

--- index2.coffee
+++ index2.coffee
@@ -3,45 +3,87 @@ nimble = require "nimble"
 drews = require "drews-mixins"
 
 define "js-tests", () ->
+      
   foo = (val) ->
     inc = () ->
       val = val + 1
       val
-    {inc}
+    setVal = (newVal) ->
+      val = newVal 
+    {inc, setVal}
+
+  foo2 =
+    inc: (self) ->
+      self.val = self.val + 1
+      self.val
+    setVal: (self, newVal) ->
+      self.val = newVal
 
   Foo = (val) ->
     @val = val
   Foo.prototype.inc = () ->
-    val = val + 1
-    val
+    @val = @val + 1
+    @val
+  Foo.prototype.setVal = (newVal) ->
+    @val = newVal
 
-  loops = 2e6 
+  loops = 1e8
 
   closure1 = () ->
-    objects = []
     for i in [0..loops]
-      objects.push foo(i) 
-      objects[i].inc()
+      myFoo = foo(i) 
+      myFoo.inc()
+    false
+
+  noPoly1 = () ->
+    for i in [0..loops]
+      myFoo = {val: i}
+      foo2.inc myFoo
+    false
 
   proto1 = () ->
-    objects = []
     for i in [0..loops]
-      objects.push new Foo(i)
-      objects[i].inc()
+      myFoo = new Foo(i) 
+      myFoo.inc()
+    false
 
   closure2 = () ->
-    objects = []
     myFoo = foo(1) 
     for i in [0..loops]
       myFoo.inc()
+    false
+
+  noPoly2 = () ->
+    myFoo = {val: 1}
+    for i in [0..loops]
+      foo2.inc myFoo
+    false
 
   proto2 = () ->
-    objects = []
     myFoo = new Foo(1)
     for i in [0..loops]
       myFoo.inc()
+    false
 
-  tests = {closure1, proto1, closure2, proto2}
+  closure3 = () ->
+    myFoo = foo(1) 
+    for i in [0..loops]
+      myFoo.setVal i
+    false
+
+  noPoly3 = () ->
+    myFoo = {val: 1}
+    for i in [0..loops]
+      foo2.setVal myFoo, i
+    false
+
+  proto3 = () ->
+    myFoo = new Foo(1)
+    for i in [0..loops]
+      myFoo.setVal i
+    false
+
+  tests = {closure1, noPoly1, proto1, closure2, noPoly2, proto2, closure3,noPoly3, proto3}
   run = () ->
     for name, test of tests 
       start = drews.time()
--- index2.js
+++ index2.js
@@ -4,71 +4,129 @@
   nimble = require("nimble");
   drews = require("drews-mixins");
   define("js-tests", function() {
-    var Foo, closure1, closure2, foo, loops, proto1, proto2, run, tests;
+    var Foo, closure1, closure2, closure3, foo, foo2, loops, noPoly1, noPoly2, noPoly3, proto1, proto2, proto3, run, tests;
     foo = function(val) {
-      var inc;
+      var inc, setVal;
       inc = function() {
         val = val + 1;
         return val;
       };
-      return {
-        inc: inc
+      setVal = function(newVal) {
+        return val = newVal;
       };
+      return {
+        inc: inc,
+        setVal: setVal
+      };
+    };
+    foo2 = {
+      inc: function(self) {
+        self.val = self.val + 1;
+        return self.val;
+      },
+      setVal: function(self, newVal) {
+        return self.val = newVal;
+      }
     };
     Foo = function(val) {
       return this.val = val;
     };
     Foo.prototype.inc = function() {
-      var val;
-      val = val + 1;
-      return val;
+      this.val = this.val + 1;
+      return this.val;
     };
-    loops = 2e6;
+    Foo.prototype.setVal = function(newVal) {
+      return this.val = newVal;
+    };
+    loops = 1e8;
     closure1 = function() {
-      var i, objects, _results;
-      objects = [];
-      _results = [];
+      var i, myFoo;
       for (i = 0; 0 <= loops ? i <= loops : i >= loops; 0 <= loops ? i++ : i--) {
-        objects.push(foo(i));
-        _results.push(objects[i].inc());
+        myFoo = foo(i);
+        myFoo.inc();
       }
-      return _results;
+      return false;
+    };
+    noPoly1 = function() {
+      var i, myFoo;
+      for (i = 0; 0 <= loops ? i <= loops : i >= loops; 0 <= loops ? i++ : i--) {
+        myFoo = {
+          val: i
+        };
+        foo2.inc(myFoo);
+      }
+      return false;
     };
     proto1 = function() {
-      var i, objects, _results;
-      objects = [];
-      _results = [];
+      var i, myFoo;
       for (i = 0; 0 <= loops ? i <= loops : i >= loops; 0 <= loops ? i++ : i--) {
-        objects.push(new Foo(i));
-        _results.push(objects[i].inc());
+        myFoo = new Foo(i);
+        myFoo.inc();
       }
-      return _results;
+      return false;
     };
     closure2 = function() {
-      var i, myFoo, objects, _results;
-      objects = [];
+      var i, myFoo;
       myFoo = foo(1);
-      _results = [];
       for (i = 0; 0 <= loops ? i <= loops : i >= loops; 0 <= loops ? i++ : i--) {
-        _results.push(myFoo.inc());
+        myFoo.inc();
       }
-      return _results;
+      return false;
+    };
+    noPoly2 = function() {
+      var i, myFoo;
+      myFoo = {
+        val: 1
+      };
+      for (i = 0; 0 <= loops ? i <= loops : i >= loops; 0 <= loops ? i++ : i--) {
+        foo2.inc(myFoo);
+      }
+      return false;
     };
     proto2 = function() {
-      var i, myFoo, objects, _results;
-      objects = [];
+      var i, myFoo;
       myFoo = new Foo(1);
-      _results = [];
       for (i = 0; 0 <= loops ? i <= loops : i >= loops; 0 <= loops ? i++ : i--) {
-        _results.push(myFoo.inc());
+        myFoo.inc();
       }
-      return _results;
+      return false;
+    };
+    closure3 = function() {
+      var i, myFoo;
+      myFoo = foo(1);
+      for (i = 0; 0 <= loops ? i <= loops : i >= loops; 0 <= loops ? i++ : i--) {
+        myFoo.setVal(i);
+      }
+      return false;
+    };
+    noPoly3 = function() {
+      var i, myFoo;
+      myFoo = {
+        val: 1
+      };
+      for (i = 0; 0 <= loops ? i <= loops : i >= loops; 0 <= loops ? i++ : i--) {
+        foo2.setVal(myFoo, i);
+      }
+      return false;
+    };
+    proto3 = function() {
+      var i, myFoo;
+      myFoo = new Foo(1);
+      for (i = 0; 0 <= loops ? i <= loops : i >= loops; 0 <= loops ? i++ : i--) {
+        myFoo.setVal(i);
+      }
+      return false;
     };
     tests = {
       closure1: closure1,
+      noPoly1: noPoly1,
       proto1: proto1,
       closure2: closure2,
-      proto2: proto2
+      noPoly2: noPoly2,
+      proto2: proto2,
+      closure3: closure3,
+      noPoly3: noPoly3,
+      proto3: proto3
     };
     run = function() {
       var end, name, start, test, _results;
